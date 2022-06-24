36448a5f22d77032a4707b2571b1f1be6ee8cd8d
coofzilla <75731066+coofzilla@users.noreply.github.com>
2022-06-25 00:56:35 +0900

    comments f/new homework

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
+//make an "App" Component, render that instead of Hello world
 root.render(<p>Hello world!</p>);

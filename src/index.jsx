4d1f626efe417ad1ea8db8003eb61e65c7a65d6b
coofzilla <jeric614@yahoo.com>
2022-06-25 23:13:13 +0900

    comments f/hw

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+//Move me into Components folder
+// then import here and render
 const App = () => {
   return <div>Hello App</div>;
 };

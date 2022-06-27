b5ff7000145cf7ce23a15105c52ac20a3a056773
coofzilla <jeric614@yahoo.com>
2022-06-27 22:38:20 +0900

    react teaching suspended while finish basics course

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+//React Teaching suspended until 경은 finishes basics Jul 1, 2022
+
 //Why did I remove the import for React???
 import { createRoot } from 'react-dom/client';
 import App from './App';
